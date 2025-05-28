da2331f004efd214de29262671e8d43394000e2a
Lomo Jean <jfleming@gmail.com>
2025-05-28 14:04:03 -0700

    Update script.js

--- script.js
+++ script.js
@@ -24,33 +24,33 @@ function imgchange() {
   // Flower randomizer
   var myImages1 = new Array();
   myImages1[1] =
-    "images/flwr2.jpeg?v=1652398743703";
+    "images/flwr2.jpeg";
   myImages1[2] =
-    "images/flwr3.jpeg?v=1652398748263";
+    "images/flwr3.jpeg";
   myImages1[3] =
-    "images/flwr4.jpeg?v=1652398753479";
+    "images/flwr4.jpeg";
   myImages1[4] =
-    "images/flwr5.jpeg?v=1652398757599";
+    "images/flwr5.jpeg";
   myImages1[5] =
-    "images/flwr6.jpeg?v=1652397689595";
+    "images/flwr6.jpeg";
     myImages1[6] =
-    "images/flwr7.jpeg?v=1652397689595";
+    "images/flwr7.jpeg";
     myImages1[7] =
-    "images/flwr8.jpeg?v=1652397689595";
+    "images/flwr8.jpeg?";
     myImages1[8] =
-    "images/flwr9.jpeg?v=1652397689595";
+    "images/flwr9.jpeg";
     myImages1[9] =
-    "images/flwr10.jpeg?v=1652397689595";
+    "images/flwr10.jpeg";
       myImages1[10] =
-    "images/flwr10.jpeg?v=1652745757077";
+    "images/flwr10.jpeg";
       myImages1[11] =
-    "images/flwr11.jpeg?v=1683663008831";
+    "images/flwr11.jpeg";
         myImages1[12] =
-    "images/flwr12.jpeg?v=1683663012746";
+    "images/flwr12.jpeg";
         myImages1[13] =
-    "images/flwr13.jpeg?v=1683663015576";
+    "images/flwr13.jpeg";
         myImages1[14] =
-    "images/flwr14.jpeg?v=1683663018065";
+    "images/flwr14.jpeg";
   var rnd = Math.floor(Math.random() * myImages1.length);
   if (rnd == 0) {
     rnd = 1;
